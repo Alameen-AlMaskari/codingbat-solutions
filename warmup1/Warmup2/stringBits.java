425de694be8c28ae8abbea5ac5ce47e09e6eae1f
Alameen <ADMIN@DESKTOP>
2025-07-28 14:13:05 +0400

    Add solution for warmup2-stringBits

--- warmup1/Warmup2/stringBits.java
+++ warmup1/Warmup2/stringBits.java
@@ -1,4 +1,10 @@
 package Warmup2;
 
-public class stringBits {
+public String stringBits(String str) {
+    String result = "";
+
+    for (int i=0; i<str.length(); i+=2) {
+        result = result + str.substring(i, i+1);
+    }
+    return result;
 }
